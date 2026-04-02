df52d7546683ea802d5bda450cb323b3ebbde587
rokubo <utokyo-okubo@g.ecc.u-tokyo.ac.jp>
2026-04-02 14:00:36 +0900

    update

--- egnn_pred/module/datamodule.py
+++ egnn_pred/module/datamodule.py
@@ -11,6 +11,7 @@ class DataModule(pl.LightningDataModule):
             batch_size,
             num_workers,
             pin_memory=False,
+            normalize=None,
     ):
         super(DataModule, self).__init__()
         self.train_dataset_path = train_dataset_path
@@ -22,10 +23,37 @@ class DataModule(pl.LightningDataModule):
         self.train_dataset = None
         self.val_dataset = None
         self.test_dataset = None
+        self.normalize = normalize
         self.save_hyperparameters()
 
     def setup(self, stage=None):
         train_data = torch.load(self.train_dataset_path, weights_only=False)
         val_data = torch.load(self.val_dataset_path, weights_only=False)
         test_data = torch.load(self.test_dataset_path, weights_only=False)
+
+        self.train_dataset = train_data
+        self.val_dataset = val_data
+        self.test_dataset = test_data
+
+    def collate_fn(self, batch):
+        pos_tensor_list = []
+        atom_type_tensor_list = []
+        spectrum_tensor_list = []
+        for data in batch:
+            pos_tensor_list.append(data.pos)
+            atom_type_tensor_list(data.x)
+            spectrum_tensor = data.spectrum_raw[0]
+            if self.normalize is None:
+                normalized_spectrum = spectrum_tensor.repeat(data.x.shape[0],1)
+            elif self.normaize == 'max':
+                max_value = spectrum_tensor.max()
+                normalized_spectrum = (spectrum_tensor / max_value).repeat(data.x.shape[0],1)
+            elif self.normalize == 'sum':
+                sum_value = spectrum_tensor.sum()
+                normalized_spectrum = (spectrum_tensor / sum_value).repeat(data.x.shape[0],1)
+            else:
+                raise ValueError(f"Invalid normalization method: {self.normalize}")
+            spectrum_tensor_list.append(normalized_spectrum)            
+
+        return pos_atom_type_tensor, spectrum_tensor, edge_index
         
